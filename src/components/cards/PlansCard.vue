695d999a61fc8ca7e24dc35b47d4a2979b7c96a5
Marco Sandri <sandrimarco98@gmail.com>
2022-03-06 14:55:14 +0100

    plans section

---
+++ src/components/cards/PlansCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="plan">
+      <div class="info">
+        <span class="tag">{{tag}}</span>
+        <h1>{{price}}</h1>
+        <p>{{frequency}}</p>
+        <p>{{description}}</p>
+      </div>
+      <div class="features">
+          <ul>
+              <li v-for="(feature,index) in features" :key="index"><font-awesome-icon icon="fa-regular fa-circle-check" class="highlight"/> {{feature}} </li>
+          </ul>
+      </div>
+      <div class="action">
+          <button class="blue-btn big-btn">Buy Now</button>
+          <div class="disclaimer">*No credit card required</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PlansCard",
+    props: {
+        tag: String,
+        price: String,
+        frequency: String,
+        description: String,
+        features: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../../assets/style/colors.scss';
+    @import '../../assets/style/general.scss';
+
+    .plan {
+        margin-top: 1rem;
+        padding: 2rem;
+        border: 1px solid $lightblueborder_btn;
+        border-radius: 10px;
+        width: 350px;
+
+        .info {
+            .tag {
+                @include lightblue-tag;
+                border-radius: 100vh;
+                padding: 0.5rem 1.25rem;
+            }
+    
+            h1 {
+                margin-bottom: 0.5rem;
+            }
+    
+            p {
+                margin: 0.5rem 0 1rem 0;
+            }
+        }
+
+        .features {
+            padding: 1rem 0;
+            border-top: 1px solid $lightblueborder_btn;
+
+            ul {
+                line-height: 1.75rem;
+                color: $lightgray_txt;
+            }
+        }
+
+        .action {
+
+            .disclaimer {
+                color: $lightgray_txt;
+                margin-top: 1rem;
+            }
+        }
+    
+    }
+</style>
--- src/components/partials/PlansComp.vue
+++ src/components/partials/PlansComp.vue
@@ -4,12 +4,64 @@
         <h2>Our rates for developer from landick team</h2>
         <p>Start working with <span class="highlight">Landrick</span> that can provide everything you need to generate awareness, drive traffic, connect.</p>
       </div>
+      <div class="plans">
+        <PlansCard v-for="(plan, index) in plans" :key="index" :tag="plan.type" :price="plan.price" :frequency="plan.frequency" :description="plan.description" :features="plan.features"/>
+      </div>
   </section>
 </template>
 
 <script>
+import PlansCard from '../cards/PlansCard.vue'
+
 export default {
-    name: 'PlansComp'
+    name: 'PlansComp',
+    components: {
+      PlansCard,
+    },
+    data() {
+      return {
+        plans: [
+          {
+            type: 'Basic',
+            price: '$9.00',
+            frequency: 'Per Month',
+            description : 'All the basics for business that are just getting started.',
+            features: [
+              'Full Access',
+              'Enhanced Security',
+              'Source Files',
+              '1 Domain Free',
+              'Enhanced Security'
+            ]
+          },
+          {
+            type: 'Business',
+            price: '$39.00',
+            frequency: 'Per Month',
+            description : 'Better for growing business that want more customers.',
+            features: [
+              'Full Access',
+              'Enhanced Security',
+              'Source Files',
+              '1 Domain Free',
+              'Enhanced Security'
+            ]
+          },{
+            type: 'Enterprise',
+            price: '$79.00',
+            frequency: 'Per Month',
+            description : 'Advanced features for pros who need more customization.',
+            features: [
+              'Full Access',
+              'Enhanced Security',
+              'Source Files',
+              '1 Domain Free',
+              'Enhanced Security'
+            ]
+          },
+        ]
+      }
+    }
 
 }
 </script>
@@ -22,5 +74,11 @@ export default {
         text-align: center;
         width:55%;
         margin: auto;
+    }
+
+    .plans {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
     }
 </style>
